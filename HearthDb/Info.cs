7b3b8035378faff6c1b44321323e47a8fcd6b22b
Alexander Zeier <epikz37@gmail.com>
2016-07-14 11:18:02 +0200

    set AssemblyVersion to Hearthstone build

--- HearthDb/Info.cs
+++ HearthDb/Info.cs
@@ -10,6 +10,5 @@ namespace HearthDb
 	public static class Info
 	{
 		public static Version HearthDbVersion => Assembly.GetExecutingAssembly().GetName().Version;
-		public static Version HearthstoneVersion => new Version(5, 0, 0, 12574);
 	}
 }

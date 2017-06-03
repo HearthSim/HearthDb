0ccfef7d4fdacb241ab81f044c3777eb4de33c5a
Alexander Zeier <alex@hearthsim.net>
2017-06-03 19:58:30 +0200

    Add DeckSerializer

---
+++ HearthDb/Deckstrings/Deck.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.Linq;
+using HearthDb.Enums;
+
+namespace HearthDb.Deckstrings
+{
+	public class Deck
+	{
+		/// <summary>
+		/// DbfId of the hero. Required.
+		/// This can be a specific hero for a class, i.e. Medivh over Jaina.
+		/// </summary>
+		public int HeroDbfId { get; set; }
+
+		/// <summary>
+		/// Dictionary of (DbfId, Count) for each card.
+		/// Needs to be a total of 30 cards to be accepted by Hearthstone.
+		/// </summary>
+		public Dictionary<int, int> CardDbfIds { get; set; } = new Dictionary<int, int>();
+
+		/// <summary>
+		/// Format of the deck. Required.
+		/// </summary>
+		public FormatType Format { get; set; }
+
+		/// <summary>
+		/// Year of the deck format. Optional.
+		/// </summary>
+		public ZodiacYear ZodiacYear { get; set; }
+
+		/// <summary>
+		/// Name of the deck. Optional.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Gets the card object for the given HeroDbfId
+		/// </summary>
+		public Card GetHero() => Cards.GetFromDbfId(HeroDbfId, false);
+
+		/// <summary>
+		/// Converts (DbfId, Count) dictionary to (CardObject, Count).
+		/// </summary>
+		public Dictionary<Card, int> GetCards() => CardDbfIds.ToDictionary(x => Cards.GetFromDbfId(x.Key), x => x.Value);
+	}
+}
---
+++ HearthDb/Deckstrings/InvalidDeckException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HearthDb.Deckstrings
+{
+	public class InvalidDeckException : Exception
+	{
+		public InvalidDeckException(string message) : base(message)
+		{
+		}
+	}
+}

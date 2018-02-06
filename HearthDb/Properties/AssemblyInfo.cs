be932fae6d356107a3e5ec84a56ad3b07f6e6b0a
Alexander Zeier <alex@hearthsim.net>
2018-02-06 19:43:41 +0100

    Update copyright year

--- HearthDb/Properties/AssemblyInfo.cs
+++ HearthDb/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HearthSim")]
 [assembly: AssemblyProduct("HearthDb")]
-[assembly: AssemblyCopyright("Copyright © HearthSim 2016")]
+[assembly: AssemblyCopyright("Copyright © HearthSim 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 

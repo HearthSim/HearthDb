aa8bbdbc500a2fbf48acb60a5f3af757bd2bffa2
Alexander Zeier <alex@hearthsim.net>
2018-04-17 21:25:38 +0200

    Add IsWild helper to Card

---
+++ HearthDb/Helper.cs
@@ -0,0 +1,21 @@
+﻿using HearthDb.Enums;
+
+namespace HearthDb
+{
+	public static class Helper
+	{
+		public static CardSet[] WildSets =
+		{
+			CardSet.PROMO,
+			CardSet.HOF,
+			CardSet.NAXX,
+			CardSet.GVG,
+			CardSet.BRM,
+			CardSet.LOE,
+			CardSet.TGT,
+			CardSet.OG,
+			CardSet.KARA,
+			CardSet.GANGS
+		};
+	}
+}

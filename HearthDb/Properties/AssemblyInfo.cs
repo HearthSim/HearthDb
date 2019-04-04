6d4fc75b28a5d2cd2c1bc5510b7ed6acdb2b53d9
Alexander Zeier <alex@hearthsim.net>
2019-04-04 12:03:47 -0700

    Bump version to 14.0.0.29933

--- HearthDb/Properties/AssemblyInfo.cs
+++ HearthDb/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("13.4.0.29349")]
-[assembly: AssemblyFileVersion("13.4.0.29349")]
+[assembly: AssemblyVersion("14.0.0.29933")]
+[assembly: AssemblyFileVersion("14.0.0.29933")]

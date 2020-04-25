5c1903bc1fd1357fe1b95cc38428eafafb67c6cc
Alexander Zeier <alex@hearthsim.net>
2020-04-25 10:51:17 -0700

    Migrate to .NET Standard/Core
    Closes #8
    
    Partially based on #9
    
    Co-authored-by: strican <strican@live.com>

--- HearthDb/Properties/AssemblyInfo.cs
+++ HearthDb/Properties/AssemblyInfo.cs
@@ -1,18 +1,6 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("HearthDb")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HearthSim")]
-[assembly: AssemblyProduct("HearthDb")]
-[assembly: AssemblyCopyright("Copyright © HearthSim 2020")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,16 +9,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7ed14243-e02b-4b94-af00-a67a62c282f0")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("17.0.2.45932")]
-[assembly: AssemblyFileVersion("17.0.2.45932")]

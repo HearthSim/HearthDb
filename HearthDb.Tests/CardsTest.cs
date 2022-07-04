28df67ba48fdc2653c3c98adda06b4f0a438e23d
Édipo da Silva Souza <edipo2s@gmail.com>
2022-07-04 20:03:40 -0300

    Add LordJaraxxus versions test

---
+++ HearthDb.Tests/CardsTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HearthDb.Tests
+{
+	[TestClass]
+	public class CardsTest
+	{
+		
+		[TestMethod]
+		public void Jaraxxus_AllVersionsAreCollectible()
+		{
+			Assert.IsTrue(Cards.Collectible.ContainsKey(CardIds.Collectible.Warlock.LordJaraxxus));
+			Assert.IsTrue(Cards.Collectible.ContainsKey(CardIds.Collectible.Warlock.LordJaraxxusCore));
+		}
+	}
+}

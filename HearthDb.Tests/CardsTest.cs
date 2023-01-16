ec07cd17259659eafe575578f12a273e3e65320a
Benedict Etzel <benedict@hearthsim.net>
2023-01-17 00:47:44 +0100

    Add Card.SecondaryRace (#26)

--- HearthDb.Tests/CardsTest.cs
+++ HearthDb.Tests/CardsTest.cs
@@ -1,3 +1,4 @@
+using HearthDb.Enums;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HearthDb.Tests
@@ -10,6 +11,30 @@ namespace HearthDb.Tests
 		{
 			Assert.IsTrue(Cards.Collectible.ContainsKey(CardIds.Collectible.Warlock.LordJaraxxus));
 			Assert.IsTrue(Cards.Collectible.ContainsKey(CardIds.Collectible.Warlock.LordJaraxxusCore));
+		}
+
+		[TestMethod]
+		public void ArgentSquire_HasNoRace()
+		{
+			var squire = Cards.Collectible[CardIds.Collectible.Neutral.ArgentSquireExpert1];
+			Assert.AreEqual(Race.INVALID, squire.Race);
+			Assert.AreEqual(Race.INVALID, squire.SecondaryRace);
+		}
+
+		[TestMethod]
+		public void MurlocWarleader_HasOneRace()
+		{
+			var murloc = Cards.Collectible[CardIds.Collectible.Neutral.MurlocWarleader];
+			Assert.AreEqual(Race.MURLOC, murloc.Race);
+			Assert.AreEqual(Race.INVALID, murloc.SecondaryRace);
+		}
+
+		[TestMethod]
+		public void UnearthedRaptor_HasTwoRaces()
+		{
+			var raptor = Cards.All[CardIds.Collectible.Rogue.UnearthedRaptor];
+			Assert.AreEqual(Race.UNDEAD, raptor.Race);
+			Assert.AreEqual(Race.BEAST, raptor.SecondaryRace);
 		}
 	}
 }

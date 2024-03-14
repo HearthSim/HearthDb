e88471eeb8298fd12ac0c581b2cba9910844a851
Benedict Etzel <benedict@hearthsim.net>
2024-03-14 11:06:49 +0100

    chore: update for Year of the Pegasus

--- HearthDb/Helper.cs
+++ HearthDb/Helper.cs
@@ -18,7 +18,8 @@ namespace HearthDb
 			CardSet.GILNEAS, CardSet.BOOMSDAY, CardSet.TROLL,
 			CardSet.DALARAN, CardSet.ULDUM, CardSet.WILD_EVENT, CardSet.DRAGONS, CardSet.YEAR_OF_THE_DRAGON,
 			CardSet.BLACK_TEMPLE, CardSet.DEMON_HUNTER_INITIATE, CardSet.SCHOLOMANCE, CardSet.DARKMOON_FAIRE,
-			CardSet.THE_BARRENS, CardSet.WAILING_CAVERNS, CardSet.STORMWIND, CardSet.ALTERAC_VALLEY
+			CardSet.THE_BARRENS, CardSet.WAILING_CAVERNS, CardSet.STORMWIND, CardSet.ALTERAC_VALLEY,
+			CardSet.THE_SUNKEN_CITY, CardSet.REVENDRETH, CardSet.RETURN_OF_THE_LICH_KING, CardSet.PATH_OF_ARTHAS,
 		};
 
 		public static CardSet[] ClassicSets = { CardSet.VANILLA };
@@ -27,15 +28,15 @@ namespace HearthDb
 		{
 			CardIds.Collectible.Deathknight.LesserSpinelSpellstone,
 			CardIds.Collectible.Demonhunter.LesserOpalSpellstone,
-			CardIds.Collectible.Druid.LesserJasperSpellstoneCorePlaceholder,
+			CardIds.Collectible.Druid.LesserJasperSpellstoneCore,
 			CardIds.Collectible.Druid.LesserJasperSpellstone,
 			CardIds.Collectible.Mage.LesserRubySpellstone,
 			CardIds.Collectible.Paladin.LesserPearlSpellstone,
-			CardIds.Collectible.Priest.LesserDiamondSpellstoneCorePlaceholder,
+			CardIds.Collectible.Priest.LesserDiamondSpellstoneCore,
 			CardIds.Collectible.Priest.LesserDiamondSpellstone,
 			CardIds.Collectible.Rogue.LesserOnyxSpellstone,
 			CardIds.Collectible.Shaman.LesserSapphireSpellstone,
-			CardIds.Collectible.Warlock.LesserAmethystSpellstoneCorePlaceholder,
+			CardIds.Collectible.Warlock.LesserAmethystSpellstoneCore,
 			CardIds.Collectible.Warlock.LesserAmethystSpellstone,
 			CardIds.NonCollectible.Neutral.TheDarkness_TheDarkness
 		};
